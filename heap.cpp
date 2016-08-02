e4db993097aaa78f6a12827f8cd5ef733ac2b175
andyferraro <andrea.ferraro@cnaf.infn.it>
2016-08-02 23:19:12 +0200

    Added arguments

--- heap.cpp
+++ heap.cpp
@@ -4,16 +4,24 @@
 #include <chrono>
 #include <thread>
 #include <string.h>
+#include "tclap/CmdLine.h"
 
 //using std::string;
 using namespace std;
-long intarraysize=3000000000;
+using namespace TCLAP;
+long defaultintarraysize=3000000000, intarraysize;
 long chararraysize=12000000000;
 chrono::milliseconds duration(5000);
 
 void writetofile(int* array, long size){
 FILE* file = fopen( "myfile.bin", "wb" );
 fwrite(array, sizeof(int), size, file);
+fclose(file);
+}
+
+void readfromfile(int* array, long size){
+FILE* file = fopen( "myfile.bin", "rb" );
+fread(array, sizeof(int), size, file);
 fclose(file);
 }
 
@@ -52,8 +60,24 @@ void sleep(chrono::milliseconds dur){
   cout << "Sleep for 1000ms" << endl;
 }
 
-int main() {
+int main(int argc, char** argv) {
+try{
   //intheaparray();
+  CmdLine cmd("By A.Ferraro", ' ', "0.1");
+  ValueArg<long> intarraysizeArg("s","size","Size of the array",false,defaultintarraysize,"long");
+  cmd.add(intarraysizeArg);
+
+  SwitchArg writeSwitch("w","write","Write the array to disk", false);
+  cmd.add( writeSwitch );
+
+  SwitchArg readSwitch("r","read","Read the array from disk", false);
+  cmd.add( writeSwitch );
+
+  cmd.parse( argc, argv );
+  intarraysize = intarraysizeArg.getValue();
+  bool write=writeSwitch.getValue();
+  bool read=writeSwitch.getValue();
+
   chrono::high_resolution_clock::time_point t1, t2;
   chrono::duration<double> time_span;
 
@@ -65,6 +89,7 @@ int main() {
   cout << "Stop step1. " ;
   cout << "It took me " << time_span.count() << " seconds." << endl;
 
+  if (write){
   cout << "Start step2" << endl;
   t1 = chrono::high_resolution_clock::now();
   writetofile(array,intarraysize);
@@ -72,7 +97,19 @@ int main() {
   time_span = chrono::duration_cast<chrono::duration<double>>(t2 - t1);
   cout << "Stop step2. " ;
   cout << "It took me " << time_span.count() << " seconds." << endl;
-  delete array;
+  //delete array;
+  }
+  
+  if (read){
+  cout << "Start step3" << endl;
+  t1 = chrono::high_resolution_clock::now();
+  readfromfile(array,intarraysize);
+  t2 = chrono::high_resolution_clock::now();
+  time_span = chrono::duration_cast<chrono::duration<double>>(t2 - t1);
+  cout << "Stop step3. " ;
+  cout << "It took me " << time_span.count() << " seconds." << endl;
+  }
+
   //sleep(duration);
   //charheaparray();
   //charstackarray();
@@ -83,6 +120,8 @@ int main() {
   //cout << "Before sleep_for" << endl;
   //this_thread::sleep_for(duration);
   //cout << "Sleep for 1000ms" << endl;
+} catch (ArgException &e)  // catch any exceptions
+        { cerr << "error: " << e.error() << " for arg " << e.argId() << endl; }
   return 0;
 }
 
